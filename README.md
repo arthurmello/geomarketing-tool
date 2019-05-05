9e40eb81a55492776635dbebde4ca53efc277ab8
Arthur Mello <arthurmello@me.com>
2019-05-05 18:49:53 +0200

    Added legend to leaflet and extra notes to README

--- README.md
+++ README.md
@@ -4,5 +4,7 @@ This is a tool for finding areas in Paris based on gender and age categories.
 Its data comes from INSEE, the French bureau of statistics.
 (Source: https://www.insee.fr/fr/statistiques/3137409#consulter)
 
+Link to the app's page: https://arthurmello.github.io/geomarketing-tool/index.html
+
 Next steps include adding more variables to the filters and updating with more
 recent surveys.
--- Shiny App/server.R
+++ Shiny App/server.R
@@ -2,9 +2,6 @@
 # SHINY APP TO FIND IRIS IN PARIS ACCORDING TO USER-SET CRITERIA
 # AUTHOR: ARTHUR MELLO
 # CREATED ON: 22/04/2019
-
-# Next steps:
-# - Improve header
 #######################################################################################
 
 # Loading libraries and Token_map_box
@@ -60,7 +57,7 @@ function(input, output, session) {
       } else {
         boolean = input[[i]]
       }
-      x = rowSums(cbind(x, (INSEE@data[,paste('P14_', sex(), i, sep = "")] * boolean)))
+      x = rowSums(cbind(x, (INSEE@data[, paste('P14_', sex(), i, sep = "")] * boolean)))
     }
     
     return(x/INSEE@data$area)
@@ -73,14 +70,17 @@ function(input, output, session) {
   
   output$map = renderLeaflet({
     leaflet() %>%  addTiles(urlTemplate = Token_map_box) %>%
-      addPolygons(data = INSEE, color='black', fillColor = ~pal()(final_score()), label = paste(final_score(), input$`0014`,INSEE@data$IRIS),
+      addPolygons(data = INSEE, color='black', fillColor = ~pal()(final_score()), label = paste(round(final_score()),"targeted people/km²"),
                   fillOpacity = 0.5, weight = 1,
                   highlightOptions = highlightOptions(color = "white", weight = 7,bringToFront = FALSE,fillOpacity = 0.5),
                   labelOptions = labelOptions(noHide = FALSE,direction = 'top',offset=c(0,0),textOnly = TRUE,
                                               style=list('color'='rgba(0,0,0,1)','font-family'= 'Arial Black','font-style'= 'bold',
                                                          'box-shadow' = '0px 0px rgba(0,0,0,0.25)','font-size' = '14px',
-                                                         'background-color'='rgba(255,255,255,0.7)','border-color' = 'rgba(0,0,0,0)')))
+                                                         'background-color'='rgba(255,255,255,0.7)','border-color' = 'rgba(0,0,0,0)'))) %>%
+      addLegend("bottomright", pal = pal(), values = final_score(),
+                title = "Targeted people/km²",
+                opacity = 1
+      )
 })
 }
-
 
