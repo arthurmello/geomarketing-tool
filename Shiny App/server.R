1e7bc147d32cb9c1788a59503adf118a5ceb985a
Arthur Mello <arthurmello@me.com>
2019-05-05 13:10:03 +0200

    First commit

---
+++ Shiny App/server.R
@@ -0,0 +1,86 @@
+#######################################################################################
+# SHINY APP TO FIND IRIS IN PARIS ACCORDING TO USER-SET CRITERIA
+# AUTHOR: ARTHUR MELLO
+# CREATED ON: 22/04/2019
+
+# Next steps:
+# - Improve header
+#######################################################################################
+
+# Loading libraries and Token_map_box
+library('tidyverse')
+library('leaflet')
+library('shiny')
+
+Token_map_box = 'https://api.mapbox.com/styles/v1/arthurmello/cjrrhiraq019v2sl8kz15p59p/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYXJ0aHVybWVsbG8iLCJhIjoiY2pycmhkcXRxMXphajQzbXRoZ2dqMTFsdiJ9.SGlFjvQl9cNE4cM5k5EnaQ'
+
+# Getting current file location
+#getCurrentFileLocation <-  function()
+#{
+#  this_file <- commandArgs() %>% 
+#    tibble::enframe(name = NULL) %>%
+#    tidyr::separate(col=value, into=c("key", "value"), sep="=", fill='right') %>%
+#    dplyr::filter(key == "--file") %>%
+#    dplyr::pull(value)
+#  if (length(this_file)==0)
+#  {
+#    this_file <- rstudioapi::getSourceEditorContext()$path
+#  }
+#  return(dirname(this_file))
+#}
+
+#setwd(getCurrentFileLocation())
+#path = dirname(getCurrentFileLocation())
+
+path = ""
+# Loading data
+load("INSEE.RData")
+
+# Generating leaflet
+function(input, output, session) {
+  
+  sex = reactive({
+    # If no sex is selected, we take the whole population for a given age range as input
+    if(input$H==TRUE & input$'F' ==FALSE) {'H'}
+    else
+    {if (input$H==FALSE & input$'F' ==TRUE) {'F'}
+        else {'POP'}}
+    })
+  
+  
+  x = c(rep(0,nrow(INSEE@data)))
+  
+  # The final_score is the population according to the given input divided by the IRIS area
+  final_score = reactive({
+    
+    for (i in c('0014','1529','3044','4559','6074','75P')){
+      # If no age range is selected, we take the whole population for a given sex as input
+      if (sum(input[[i]])==0){
+        boolean = c(1,1,1,1,1,1) 
+      } else {
+        boolean = input[[i]]
+      }
+      x = rowSums(cbind(x, (INSEE@data[,paste('P14_', sex(), i, sep = "")] * boolean)))
+    }
+    
+    return(x/INSEE@data$area)
+  })
+  
+  # The polygon colors will be given according to the final_score
+  pal <- reactive({
+    colorNumeric(palette = "YlOrRd", domain = final_score())
+  })
+  
+  output$map = renderLeaflet({
+    leaflet() %>%  addTiles(urlTemplate = Token_map_box) %>%
+      addPolygons(data = INSEE, color='black', fillColor = ~pal()(final_score()), label = paste(final_score(), input$`0014`,INSEE@data$IRIS),
+                  fillOpacity = 0.5, weight = 1,
+                  highlightOptions = highlightOptions(color = "white", weight = 7,bringToFront = FALSE,fillOpacity = 0.5),
+                  labelOptions = labelOptions(noHide = FALSE,direction = 'top',offset=c(0,0),textOnly = TRUE,
+                                              style=list('color'='rgba(0,0,0,1)','font-family'= 'Arial Black','font-style'= 'bold',
+                                                         'box-shadow' = '0px 0px rgba(0,0,0,0.25)','font-size' = '14px',
+                                                         'background-color'='rgba(255,255,255,0.7)','border-color' = 'rgba(0,0,0,0)')))
+})
+}
+
+
---
+++ Shiny App/ui.R
@@ -0,0 +1,43 @@
+#######################################################################################
+# SHINY APP TO FIND IRIS IN PARIS ACCORDING TO USER-SET CRITERIA
+# AUTHOR: ARTHUR MELLO
+# CREATED ON: 22/04/2019
+#######################################################################################
+
+# Loading libraries
+library("leaflet")
+library("shiny")
+library("shinythemes")
+
+fluidPage(titlePanel("Find your target market in Paris:"),
+          sidebarLayout(
+            sidebarPanel(width = 3,
+                         h4('Gender'),
+                         checkboxInput('H', 'Men', value = FALSE),
+                         checkboxInput('F', 'Women', value = FALSE),
+                         
+                         h4('Age'),
+                         checkboxInput('0014', '0 to 14 years', value = FALSE),
+                         checkboxInput('1529', '15 to 29 years', value = FALSE),
+                         checkboxInput('3044', '30 to 44 years', value = FALSE),
+                         checkboxInput('4559', '45 to 59 years', value = FALSE),
+                         checkboxInput('6074', '60 to 74 years', value = FALSE),
+                         checkboxInput('75P', 'more than 75 years', value = FALSE)
+            ),
+            mainPanel(bootstrapPage(div(class="outer",
+                                        shiny::tags$style(
+                                          type ="text/css", ".outer {position: fixed; top: 63px;
+                                          left: 240px; right: 0; bottom: 0; overflow: hidden;
+                                          padding: 0}"),leafletOutput("map", width="100%", height="100%")
+                                        
+                                        ))
+            )
+            )
+            )
+
+
+
+
+
+
+
